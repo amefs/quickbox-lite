6e99bdd70588147a19aad5131807b34cddff11b6
tautcony <tautcony@gmail.com>
2020-12-13 23:45:53 +0800

    chore: upgrade deps

--- setup/dashboard/ws/.eslintrc.js
+++ setup/dashboard/ws/.eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "project": "./tsconfig.json",
+        "tsconfigRootDir": __dirname,
         "sourceType": "module"
     },
     "plugins": [
