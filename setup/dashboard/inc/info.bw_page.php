334cf825453dfda7d908c39eabf297ff1be3d4cc
TautCony <tautcony@gmail.com>
2021-08-10 14:45:49 +0800

    fix: bandwidth option translation

--- setup/dashboard/inc/info.bw_page.php
+++ setup/dashboard/inc/info.bw_page.php
@@ -1,8 +1,8 @@
 <?php
 
 $bw_pages = [
-    ['key' => 's', 'title' => 'BW_PAGE_SUMMARY'],
-    ['key' => 'h', 'title' => 'BW_PAGE_HOUR'],
-    ['key' => 'd', 'title' => 'BW_PAGE_DAY'],
-    ['key' => 'm', 'title' => 'BW_PAGE_MONTH'],
+    ['key' => 's', 'title' => 'Top 10 days'],
+    ['key' => 'h', 'title' => 'Last 24 hours'],
+    ['key' => 'd', 'title' => 'Last 30 days'],
+    ['key' => 'm', 'title' => 'Last 12 months'],
 ];
